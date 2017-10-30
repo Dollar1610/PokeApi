7c57b3a354c94a9eafe681f14772183aa6585e0d
Unknown <Dollar16.10.91@mail.ru>
2017-10-30 18:11:04 +0500

    Added pagination

--- src/App.js
+++ src/App.js
@@ -258,11 +258,11 @@ class App extends Component {
     return (
         <div className='container'>
           <Button onClick={this.save} bsStyle='primary'>Add Pokemon</Button>
-            <BootstrapTable data={ this.state.pokemons } borderd = {true}>
+            <BootstrapTable data={ this.state.pokemons } borderd = {true} pagination={true} options={ {sizePerPage:5} }>
                 <TableHeaderColumn dataField='id' filter={ { type: 'TextFilter', delay: 1000 } } isKey>#</TableHeaderColumn>
                 <TableHeaderColumn dataField='name' filter={ { type: 'TextFilter', delay: 1000 } } >Name</TableHeaderColumn>
                 <TableHeaderColumn dataField='image'  dataFormat={imageFormatter}>Avatar</TableHeaderColumn>
-                <TableHeaderColumn dataField='type' filterFormatted dataFormat={ enumFormatter } formatExtraData={ pokemonsType }
+                <TableHeaderColumn dataField='type'  formatExtraData={ pokemonsType }
                                    filter={ {type: 'SelectFilter', options: pokemonsType } } >Type</TableHeaderColumn>
                 <TableHeaderColumn dataField='abilities' filter={ { type: 'TextFilter', delay: 1000 } } >Abilities</TableHeaderColumn>
             </BootstrapTable>
