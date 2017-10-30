ad063b7fe11e06481d51eb6a40841170e1526948
Unknown <Dollar16.10.91@mail.ru>
2017-10-30 20:11:03 +0500

    final commit №2

--- src/App.js
+++ src/App.js
@@ -1,211 +1,11 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
 import './bootstrap/css/bootstrap.min.css';
-import '../node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
+import 'react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
 import {BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 import imageFormatter from './imageFormatter';
 import enumFormatter from './enumFormatter';
 
-let    pokemon_attr0 = {
-    id: '',
-    image:'',
-    name:'',
-    type:'',
-    abilities:'',
-    speed:'',
-    color:'',
-    habitats:''
-};
-let    pokemon_attr1 = {
-        id: '',
-        image:'',
-        name:'',
-        type:'',
-        abilities:'',
-        speed:'',
-        color:'',
-        habitats:''
-    };
-let    pokemon_attr2 = {
-        id: '',
-        image:'',
-        name:'',
-        type:'',
-        abilities:'',
-        speed:'',
-        color:'',
-        habitats:''
-    };
-let    pokemon_attr3 = {
-        id: '',
-        image:'',
-        name:'',
-        type:'',
-        abilities:'',
-        speed:'',
-        color:'',
-        habitats:''
-    };
-let    pokemon_attr4 = {
-    id: '',
-    image:'',
-    name:'',
-    type:'',
-    abilities:'',
-    speed:'',
-    color:'',
-    habitats:''
-};
-let    pokemon_attr5 = {
-    id: '',
-    name:'',
-    avatar:'',
-    type:'',
-    abilities:'',
-    speed:'',
-    color:'',
-    habitats:''
-};
-let    pokemon_attr6 = {
-    id: '',
-    image:'',
-    name:'',
-    type:'',
-    abilities:'',
-    speed:'',
-    color:'',
-    habitats:''
-};
-let    pokemon_attr7 = {
-    id: '',
-    image:'',
-    name:'',
-    type:'',
-    abilities:'',
-    speed:'',
-    color:'',
-    habitats:''
-};
-let    pokemon_attr8 = {
-    id: '',
-    image:'',
-    name:'',
-    type:'',
-    abilities:'',
-    speed:'',
-    color:'',
-    habitats:''
-};
-let    pokemon_attr9 = {
-    id: '',
-    image:'',
-    name:'',
-    type:'',
-    abilities:'',
-    speed:'',
-    color:'',
-    habitats:''
-};
-let    pokemon_attr10 = {
-    id: '',
-    image:'',
-    name:'',
-    type:'',
-    abilities:'',
-    speed:'',
-    color:'',
-    habitats:''
-};
-let    pokemon_attr11 = {
-    id: '',
-    image:'',
-    name:'',
-    type:'',
-    abilities:'',
-    speed:'',
-    color:'',
-    habitats:''
-};
-let    pokemon_attr12 = {
-    id: '',
-    image:'',
-    name:'',
-    type:'',
-    abilities:'',
-    speed:'',
-    color:'',
-    habitats:''
-};
-let    pokemon_attr13 = {
-    id: '',
-    image:'',
-    name:'',
-    type:'',
-    abilities:'',
-    speed:'',
-    color:'',
-    habitats:''
-};
-let    pokemon_attr14 = {
-    id: '',
-    image:'',
-    name:'',
-    type:'',
-    abilities:'',
-    speed:'',
-    color:'',
-    habitats:''
-};
-let    pokemon_attr15 = {
-    id: '',
-    image:'',
-    name:'',
-    type:'',
-    abilities:'',
-    speed:'',
-    color:'',
-    habitats:''
-};
-let    pokemon_attr16 = {
-    id: '',
-    image:'',
-    name:'',
-    type:'',
-    abilities:'',
-    speed:'',
-    color:'',
-    habitats:''
-};
-let    pokemon_attr17 = {
-    id: '',
-    image:'',
-    name:'',
-    type:'',
-    abilities:'',
-    speed:'',
-    color:'',
-    habitats:''
-};
-let    pokemon_attr18 = {
-    id: '',
-    image:'',
-    name:'',
-    type:'',
-    abilities:'',
-    speed:'',
-    color:'',
-    habitats:''
-};
-let    pokemon_attr19 = {
-    id: '',
-    image:'',
-    name:'',
-    type:'',
-    abilities:'',
-    speed:'',
-    color:'',
-    habitats:''
-};
 
 const pokemonsType = {
     'normal': 'normal',
@@ -219,7 +19,7 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            pokemons: [pokemon_attr0,pokemon_attr1,pokemon_attr2,pokemon_attr3,pokemon_attr4,pokemon_attr5,pokemon_attr6,pokemon_attr7,pokemon_attr8,pokemon_attr9,pokemon_attr10,pokemon_attr11,pokemon_attr12,pokemon_attr13,pokemon_attr14,pokemon_attr15,pokemon_attr16,pokemon_attr17,pokemon_attr18,pokemon_attr19]
+            pokemons: []
         };
 
         this.save=this.save.bind(this);
@@ -236,15 +36,15 @@ class App extends Component {
                 .then((response) => {
                         response.json()
                             .then((data) => {
-                                this.setState((prevState) => {
-                                    prevState.pokemons[i].id = i;
-                                    prevState.pokemons[i].name = data.name;
-                                    prevState.pokemons[i].image = data.sprites.front_default;
-                                    prevState.pokemons[i].type = data.types[0].type.name;
-                                    prevState.pokemons[i].abilities = data.abilities[0].ability.name;
-                                    return prevState;
-                                });
-
+                            const pokemons = this.state.pokemons;
+                            pokemons.push({
+                                id: i,
+                                name: data.name,
+                                image: data.sprites.front_default,
+                                type: data.types[0].type.name,
+                                abilities: data.abilities[0].ability.name,
+                            });
+                            this.setState({pokemons: pokemons});
                             });
                     }
                 );
